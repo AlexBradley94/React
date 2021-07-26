511268ca4e5cefe5fba1d017b3296dbc3f8035da
AlexBradley94 <agbradley82@gmail.com>
2021-07-26 17:25:41 +0100

    Use of states

--- expense-system/src/components/Expenses/ExpenseItem.js
+++ expense-system/src/components/Expenses/ExpenseItem.js
@@ -1,19 +1,34 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
-  
+const ExpenseItem = (props) => {
 
+  //Array deconstructor assigned to a const,
+  //the first element is the current value,
+  //the second is the new set value.
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    //Pass new value with argument in the above second value
+    //of array deconstructor
+    setTitle('updated');
+  };
+
+  //When calling the function within the onClick on line 20
+  //don't need the () otherwise it will call when it is parsed
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
